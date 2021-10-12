3627827a99e430c2dd7e3adbc5fcfb20412dff84
duonghieu1 <92351964+duonghieu1@users.noreply.github.com>
2021-10-12 11:18:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Giáp ơi!!',
+    introDesc: `Cậu phải chọn thật lòng đó nhaaaaa ❤`,
+    btnIntro: 'CLICK <3333333',
+    title: 'Phải chăng cậu đã yêu tớ ngay từ cái nhìn đầu tiên ❤ ',
+    desc: 'Phải chăng cậu đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Yêu mất rồi <33',
+    btnNo: 'Không, bạn bè à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu tớ vậy❤',
+    btnReply: 'Gửi cho tớ nha :3',
+    reply: 'Tại vì cậu đẹp trai ó <33333333',
+    mess: 'Cậu làm bạn gái tớ nhaa ',
+    messDesc: 'Nhắn cho tớ nhé.',
+    btnAccept: 'Okiiiii lun ❤',
+    messLink: 'https://www.messenger.com/t/100026579676090/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
